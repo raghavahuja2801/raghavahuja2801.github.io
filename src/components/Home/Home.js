2950d48894e50993704f5cb47df91e26d04a37ba
Raghav Ahuja <raghavahuja@Raghavs-MacBook-Pro.local>
2024-10-30 00:02:11 -0700

    updated alt texts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -33,7 +33,7 @@ function Home() {
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
                 src={homeLogo}
-                alt="home pic"
+                alt="home picture"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
               />
